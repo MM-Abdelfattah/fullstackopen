9075011593d3752cf631f842c185e62e0b7ccb4b
Mohamed Abdelfattah <mohamed.training371@gmail.com>
2026-04-06 10:18:58 +0200

    blog

--- blog-project/app.js
+++ blog-project/app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const blogsRouter = require("./controllers/blogs.js");
+
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 const app = express();
 
-const mongoUrl = "mongodb://127.0.0.1:27017/bloglist";
-
-mongoose.connect(mongoUrl);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/bloglist")
+  .then(() => console.log("connected to MongoDB"))
+  .catch((error) => console.log("error connecting to MongoDB:", error.message));
 
 app.use(express.json());
+
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
