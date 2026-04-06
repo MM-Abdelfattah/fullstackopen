8c62a1d18880b2748fbb302fcb808c42444178a9
Mohamed Abdelfattah <mohamed.training371@gmail.com>
2026-04-06 11:46:21 +0200

    blog

--- blog-project/controllers/blogs.js
+++ blog-project/controllers/blogs.js
@@ -3,25 +3,59 @@ const Blog = require("../models/blog");
 const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const search = request.query.search;
+  const { search, author, sortBy, order, page, limit } = request.query;
 
   let query = {};
 
   if (search) {
-    query = {
-      title: {
-        $regex: search,
-        $options: "i",
-      },
+    query.title = {
+      $regex: search,
+      $options: "i",
     };
   }
 
-  const blogs = await Blog.find(query).populate("user", {
-    username: 1,
-    name: 1,
-  });
+  if (author) {
+    query.author = author;
+  }
 
-  response.json(blogs);
+  let sortObject = {};
+
+  if (sortBy) {
+    if (sortBy !== "likes") {
+      return response.status(400).json({
+        error: "unsupported sort field",
+      });
+    }
+
+    sortObject[sortBy] = order === "asc" ? 1 : -1;
+  }
+
+  const pageNumber = parseInt(page) || 1;
+  const pageSize = parseInt(limit) || 10;
+
+  const skip = (pageNumber - 1) * pageSize;
+
+  const totalBlogs = await Blog.countDocuments(query);
+
+  const blogs = await Blog.find(query)
+    .populate("user", {
+      username: 1,
+      name: 1,
+    })
+    .sort(sortObject)
+    .skip(skip)
+    .limit(pageSize);
+
+  response.json({
+    pagination: {
+      currentPage: pageNumber,
+      pageSize: pageSize,
+      totalBlogs: totalBlogs,
+      totalPages: Math.ceil(totalBlogs / pageSize),
+    },
+
+    data: blogs,
+  });
 });
 
 blogsRouter.post("/", async (request, response) => {
@@ -40,6 +74,7 @@ blogsRouter.post("/", async (request, response) => {
   const savedBlog = await blog.save();
 
   user.blogs = user.blogs.concat(savedBlog._id);
+
   await user.save();
 
   response.status(201).json(savedBlog);
@@ -50,16 +85,20 @@ blogsRouter.patch("/:id/like", async (request, response) => {
     const blog = await Blog.findById(request.params.id);
 
     if (!blog) {
-      return response.status(404).json({ error: "blog not found" });
+      return response.status(404).json({
+        error: "blog not found",
+      });
     }
 
-    blog.likes = blog.likes + 1;
+    blog.likes += 1;
 
     const updatedBlog = await blog.save();
 
     response.status(200).json(updatedBlog);
   } catch (error) {
-    response.status(400).json({ error: "malformed id" });
+    response.status(400).json({
+      error: "malformed id",
+    });
   }
 });
 
